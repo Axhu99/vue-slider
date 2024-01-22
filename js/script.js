8bef65846dd2672974c79f26ff5f5cc5b0d79eac
Axhu99 <xhuliodreni@icloud.com>
2024-01-22 17:29:06 +0100

    add swich picture

--- js/script.js
+++ js/script.js
@@ -4,7 +4,24 @@ const app = createApp({
     data:() =>({
         destinations,
         currentActiveIndex: 0
-    })
+    }),
+    methods:{
+        goBack(){
+            if(this.currentActiveIndex === 0){
+                this.currentActiveIndex = this.destinations.length - 1
+            }else{
+                this.currentActiveIndex--;
+            }
+        },
+
+        goNext(){
+            if(this.currentActiveIndex === this.destinations.length - 1){
+                this.currentActiveIndex = 0
+            }else{
+                this.currentActiveIndex++;
+            }
+        }
+    }
 });
 
 app.mount('#root');
